98422f8e58a0ca8c40c0cfe4b2be22c4c6674abf
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-10-23 09:48:55 +0900

    Changed App componet to Hello component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Hello from './Hello';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'));
