a8bddd5e4718c5a020ceea08a89d03bc213425f6
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-10-23 15:19:01 +0900

    Changed export of index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Hello from './Hello';
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
+export { default as Hello } from './Hello';
